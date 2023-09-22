ddaa6f0d550ebd0ca5d1f38d298c6f8d1d19a9bf
francarrasc0 <fran.carrascolozano@gmail.com>
2023-09-22 03:39:08 -0300

    Evento onclick

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage(element){
+    alert("Ninja was liked");
+}
+
+function logOut(element){
+    element.innerText = "Logout";
+}
+
+function hide(element){
+    element.remove();
+}
